937e2cfe09e9a28d5ff8d8d3143ebf5b9eac64a2
Mikael Jagan <jaganmn@mcmaster.ca>
2024-08-22 11:28:16 -0600

    virtual class 'flint'

--- R/00_Class.R
+++ R/00_Class.R
@@ -1,17 +1,11 @@
-setClass("fmpz",
-         slots = c(length = "numeric", x = "externalptr"))
+setClass("flint",
+         contains = "VIRTUAL",
+         slots = c(length = "numeric", x = "externalptr"),
+         prototype = list(length = integer(0L)))
 
-setClass("fmpq",
-         slots = c(length = "numeric", x = "externalptr"))
-
-setClass("mag",
-         slots = c(length = "numeric", x = "externalptr"))
-
-setClass("arf",
-         slots = c(length = "numeric", x = "externalptr"))
-
-setClass("arb",
-         slots = c(length = "numeric", x = "externalptr"))
-
-setClass("acb",
-         slots = c(length = "numeric", x = "externalptr"))
+setClass("fmpz", contains = "flint")
+setClass("fmpq", contains = "flint")
+setClass( "mag", contains = "flint")
+setClass( "arf", contains = "flint")
+setClass( "arb", contains = "flint")
+setClass( "acb", contains = "flint")
