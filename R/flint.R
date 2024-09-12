32447db0918eb1af7fa7c49e88c5001e5e8b9b78
Mikael Jagan <jaganmn@mcmaster.ca>
2024-09-12 19:11:16 -0400

    methods for 'as.*' calling 'as.vector'

--- R/flint.R
+++ R/flint.R
@@ -1,3 +1,31 @@
+setMethod("as.raw",
+          c(x = "flint"),
+          function (x     ) as.vector(x, "raw"))
+
+setMethod("as.logical",
+          c(x = "flint"),
+          function (x, ...) as.vector(x, "logical"))
+
+setMethod("as.integer",
+          c(x = "flint"),
+          function (x, ...) as.vector(x, "integer"))
+
+setMethod("as.double",
+          c(x = "flint"),
+          function (x, ...) as.vector(x, "double"))
+
+setMethod("as.complex",
+          c(x = "flint"),
+          function (x, ...) as.vector(x, "complex"))
+
+setMethod("as.list",
+          c(x = "flint"),
+          function (x, ...) as.vector(x, "list"))
+
+setMethod("as.expression",
+          c(x = "flint"),
+          function (x, ...) as.vector(x, "expression"))
+
 setMethod("length",
           c(x = "flint"),
           function (x) .Call(R_flint_length, x))
