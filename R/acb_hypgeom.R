fe56273d40c41d86c7d206c46268530d1a35cc95
Mikael Jagan <jaganmn@mcmaster.ca>
2024-09-27 11:05:40 -0400

    typo

--- R/acb_hypgeom.R
+++ R/acb_hypgeom.R
@@ -1,7 +1,7 @@
 acb_hypgeom_2f1 <-
 function (a, b, c, z, flags = 0L, prec = 53L) {
     res <- flintNew("acb")
-    .Call(R_flint_acb_hypgeom_2f1, res, as(a, "acb"), as(b, "acb"), as(c, "acb"), as(c, "acb"), flags, as(prec, "slong"))
+    .Call(R_flint_acb_hypgeom_2f1, res, as(a, "acb"), as(b, "acb"), as(c, "acb"), as(z, "acb"), flags, as(prec, "slong"))
     res
 }
 
