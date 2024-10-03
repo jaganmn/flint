dd195d661b67ee8f1ef6f37c49b5d32188ca3524
Mikael Jagan <jaganmn@mcmaster.ca>
2024-10-03 13:28:16 -0400

    finish implementing and document 'flintVersion'

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,23 @@
 flintBits <- function () .Call(R_flint_bits)
-flintVersion <- function () .Call(R_flint_version)
+flintVersion <- function () {
+    n <- .Call(R_flint_version)
+    v <- .mapply(function(n, p, b, class) {
+                     r <- integer(p)
+                     while (p > 0L) {
+                         r[p] <- tmp <- n %% b
+                         n <- (n - tmp) %/% b
+                         p <- p - 1L
+                     }
+                     v <- list(r)
+                     class(v) <- c(class, "numeric_version")
+                     v
+                 },
+                 list(n = n, p = 3L, b = 256L,
+                      class = list("package_version", NULL, NULL, NULL, NULL, NULL, NULL)),
+                 NULL)
+    names(v) <- names(n)
+    v
+}
 flintClass <- function (object) .Call(R_flint_class, object)
 flintNew <- function (class) .Call(R_flint_new, class)
 flintValid <- function (object) .Call(R_flint_valid, object)
--- src/version.c
+++ src/version.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <gmp.h>
 #include <mpfr.h>
 #include <flint/flint.h>
@@ -14,25 +15,37 @@ SEXP R_flint_bits(void)
 
 SEXP R_flint_version(void)
 {
-	SEXP ans, nms;
-	PROTECT(ans = Rf_allocVector(INTSXP, 4));
+	int libflint[3], libmpfr[3], libgmp[3];
+	sscanf(flint_version,
+	       "%d.%d.%d", &libflint[0], &libflint[1], &libflint[2]);
+	sscanf(mpfr_get_version(),
+	       "%d.%d.%d", &libmpfr[0], &libmpfr[1], &libmpfr[2]);
+	sscanf(gmp_version,
+	       "%d.%d.%d", &libgmp[0], &libgmp[1], &libgmp[2]);
+	SEXP ans = PROTECT(Rf_allocVector(INTSXP, 7));
 	INTEGER(ans)[0] = R_Version(PACKAGE_VERSION_MAJOR,
 	                            PACKAGE_VERSION_MINOR,
 	                            PACKAGE_VERSION_PATCHLEVEL);
 	INTEGER(ans)[1] = R_Version(__FLINT_VERSION,
 	                            __FLINT_VERSION_MINOR,
 	                            __FLINT_VERSION_PATCHLEVEL);
-	INTEGER(ans)[2] = R_Version(MPFR_VERSION_MAJOR,
+	INTEGER(ans)[2] = R_Version(libflint[0], libflint[1], libflint[2]);
+	INTEGER(ans)[3] = R_Version(MPFR_VERSION_MAJOR,
 	                            MPFR_VERSION_MINOR,
 	                            MPFR_VERSION_PATCHLEVEL);
-	INTEGER(ans)[3] = R_Version(__GNU_MP_VERSION,
+	INTEGER(ans)[4] = R_Version(libmpfr[0], libmpfr[1], libmpfr[2]);
+	INTEGER(ans)[5] = R_Version(__GNU_MP_VERSION,
 	                            __GNU_MP_VERSION_MINOR,
 	                            __GNU_MP_VERSION_PATCHLEVEL);
-	PROTECT(nms = Rf_allocVector(STRSXP, 4));
+	INTEGER(ans)[6] = R_Version(libgmp[0], libgmp[1], libgmp[2]);
+	SEXP nms = PROTECT(Rf_allocVector(STRSXP, 7));
 	SET_STRING_ELT(nms, 0, Rf_mkChar("package"));
-	SET_STRING_ELT(nms, 1, Rf_mkChar("flint"));
-	SET_STRING_ELT(nms, 2, Rf_mkChar("mpfr"));
-	SET_STRING_ELT(nms, 3, Rf_mkChar("gmp"));
+	SET_STRING_ELT(nms, 1, Rf_mkChar("flint.h"));
+	SET_STRING_ELT(nms, 2, Rf_mkChar("libflint"));
+	SET_STRING_ELT(nms, 3, Rf_mkChar("mpfr.h"));
+	SET_STRING_ELT(nms, 4, Rf_mkChar("libmpfr"));
+	SET_STRING_ELT(nms, 5, Rf_mkChar("gmp.h"));
+	SET_STRING_ELT(nms, 6, Rf_mkChar("libgmp"));
 	Rf_setAttrib(ans, R_NamesSymbol, nms);
 	UNPROTECT(2);
 	return ans;
