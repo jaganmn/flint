5cff120f12da33e858fea3127d6eed6c28556df6
Mikael Jagan <jaganmn@mcmaster.ca>
2024-11-28 06:50:21 -0500

    Ops, Math, Math2, Summary, Complex for classes 'slong', 'ulong'

--- R/slong.R
+++ R/slong.R
@@ -24,3 +24,41 @@ setMethod("format",
           c(x = "slong"),
           function (x, base = 10L, ...)
               .Call(R_flint_slong_format, x, base))
+
+setMethod("+",
+          c(e1 = "slong", e2 = "missing"),
+          function (e1, e2)
+              +as(e1, "fmpz"))
+
+setMethod("-",
+          c(e1 = "slong", e2 = "missing"),
+          function (e1, e2)
+              -as(e1, "fmpz"))
+
+setMethod("Ops",
+          c(e1 = "slong", e2 = "slong"),
+          function (e1, e2)
+              get(.Generic, mode = "function")(as(e1, "fmpz"), as(e2, "fmpz")))
+
+setMethod("Math",
+          c(x = "slong"),
+          function (x)
+              get(.Generic, mode = "function")(as(x, "fmpz")))
+
+setMethod("Math2",
+          c(x = "slong"),
+          function (x, digits) {
+              if (missing(digits))
+                  get(.Generic, mode = "function")(as(x, "fmpz"))
+              else get(.Generic, mode = "function")(as(x, "fmpz"), digits = digits)
+          })
+
+setMethod("Summary",
+          c(x = "slong"),
+          function (x, ..., na.rm = FALSE)
+              get(.Generic, mode = "function")(as(x, "fmpz"), ..., na.rm = na.rm))
+
+setMethod("Complex",
+          c(z = "slong"),
+          function (z)
+              get(.Generic, mode = "function")(as(z, "fmpz")))
--- R/ulong.R
+++ R/ulong.R
@@ -24,3 +24,41 @@ setMethod("format",
           c(x = "ulong"),
           function (x, base = 10L, ...)
               .Call(R_flint_ulong_format, x, base))
+
+setMethod("+",
+          c(e1 = "ulong", e2 = "missing"),
+          function (e1, e2)
+              +as(e1, "fmpz"))
+
+setMethod("-",
+          c(e1 = "ulong", e2 = "missing"),
+          function (e1, e2)
+              -as(e1, "fmpz"))
+
+setMethod("Ops",
+          c(e1 = "ulong", e2 = "ulong"),
+          function (e1, e2)
+              get(.Generic, mode = "function")(as(e1, "fmpz"), as(e2, "fmpz")))
+
+setMethod("Math",
+          c(x = "ulong"),
+          function (x)
+              get(.Generic, mode = "function")(as(x, "fmpz")))
+
+setMethod("Math2",
+          c(x = "ulong"),
+          function (x, digits) {
+              if (missing(digits))
+                  get(.Generic, mode = "function")(as(x, "fmpz"))
+              else get(.Generic, mode = "function")(as(x, "fmpz"), digits = digits)
+          })
+
+setMethod("Summary",
+          c(x = "ulong"),
+          function (x, ..., na.rm = FALSE)
+              get(.Generic, mode = "function")(as(x, "fmpz"), ..., na.rm = na.rm))
+
+setMethod("Complex",
+          c(z = "ulong"),
+          function (z)
+              get(.Generic, mode = "function")(as(z, "fmpz")))
