190ffc615dab2ef908adc7396fc58046dc84f72e
Mikael Jagan <jaganmn@mcmaster.ca>
2025-02-19 19:21:04 -0500

    after more thought

--- src/noreturn.h
+++ src/noreturn.h
@@ -1,6 +1,6 @@
 /* flint-3.1.x/src/flint.h.in is broken due to nonportable */
-/* use of keyword macro 'noreturn'                         */
-#if __STDC_VERSION__ >= 201112L && __STDC_VERSION__ < 202311L
+/* use of keyword '_Noreturn' and keyword macro 'noreturn' */
+#if defined __STDC_VERSION__ && __STDC_VERSION__ >= 201112L
 # include <stdnoreturn.h>
 #else
 # undef _Noreturn
