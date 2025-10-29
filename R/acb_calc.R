16350a0acdc66f1128f69bd6f924d0c48e8199f8
Mikael Jagan <jaganmn@mcmaster.ca>
2025-10-29 08:11:05 -0400

    s/abs.tol/atol/, s/rel.tol/rtol/

--- R/acb_calc.R
+++ R/acb_calc.R
@@ -27,13 +27,13 @@ function (deg.limit, eval.limit, depth.limit, use.heap, verbose) {
 }
 
 acb_integrate <-
-function (func, a, b, param = NULL, rel.tol = NULL, abs.tol = NULL,
+function (func, a, b, param = NULL, rtol = NULL, atol = NULL,
           control = NULL, prec = flintPrec()) {
     res <- flintNew("acb")
     .Call(R_flint_acb_calc_integrate, res,
           .integrate.func(func), param, as(a, "acb"), as(b, "acb"),
-          if (!is.null(rel.tol)) as(-floor(log2(rel.tol)), "slong"),
-          if (!is.null(abs.tol)) as(abs.tol, "mag"),
+          if (!is.null(rtol)) as(-floor(log2(rtol)), "slong"),
+          if (!is.null(atol)) as(atol, "mag"),
           if (!is.null(control)) do.call(.integrate.control, control),
           as(prec, "slong"))
     res
