97d0348d23cd26d424d0f747459c4e14803e2dbf
Mikael Jagan <jaganmn@mcmaster.ca>
2025-11-05 03:33:36 -0500

    coerce 'rtol' to "mag"

--- R/acb_calc.R
+++ R/acb_calc.R
@@ -4,7 +4,7 @@ function (func, a, b, param = NULL, rtol = NULL, atol = NULL,
     res <- flintNew("acb")
     .Call(R_flint_acb_calc_integrate, res,
           .integrate.func(func), param, as(a, "acb"), as(b, "acb"),
-          if (!is.null(rtol)) as(-floor(log2(rtol)), "slong"),
+          if (!is.null(rtol)) as(-floor(log2(as(rtol, "mag"))), "slong"),
           if (!is.null(atol)) as(atol, "mag"),
           if (!is.null(control)) do.call(.integrate.control, control),
           as(prec, "slong"))
--- R/arb_calc.R
+++ R/arb_calc.R
@@ -32,7 +32,7 @@ function (func, a, b, param = NULL, rtol = NULL, atol = NULL,
     res <- flintNew("arb")
     .Call(R_flint_arb_calc_integrate, res,
           .integrate.func(func), param, as(a, "arb"), as(b, "arb"),
-          if (!is.null(rtol)) as(-floor(log2(rtol)), "slong"),
+          if (!is.null(rtol)) as(-floor(log2(as(rtol, "mag"))), "slong"),
           if (!is.null(atol)) as(atol, "mag"),
           if (!is.null(control)) do.call(.integrate.control, control),
           as(prec, "slong"))
